4255f1d3637aeff7aefa0c775e401a9a883dd391
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-01-16 09:16:29 +0530

    removed rate limit middleware

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require("../controllers/authController");
 const loginLimiter = require("../middleware/loginLimiter");
 
-router.route('/').post(loginLimiter, authController.login);
+router.route('/').post(authController.login);
 router.route('/register').post(authController.register);
 router.route('/refresh').get(authController.refresh);
 router.route('/logout').post(authController.logout);
