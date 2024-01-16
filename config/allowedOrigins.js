1d6e88c2bd098a6911b7ae19d8bb756e4e60a912
Md Abid Hussain <abid0hussain1733@gmail.com>
2024-01-16 09:11:21 +0530

    updated cors

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -2,7 +2,9 @@ const allowedOrigins = [
     'http://127.0.0.1:5500',
     'http://localhost:3500',
     'http://localhost:5173',
-    'https://retail-frontend-teal.vercel.app/'
+    'https://retail-frontend-teal.vercel.app',
+    'retail-frontend-git-main-md-abid-hussain.vercel.app',
+    'retail-frontend-dwpu4jlb1-md-abid-hussain.vercel.app'
 ]
 
 module.exports = allowedOrigins;
